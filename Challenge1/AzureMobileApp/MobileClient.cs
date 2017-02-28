377072debc5a62059411b5067a6c81afae5ae732
ben ishiyama-levy <ben@tippstr.com>
2017-02-28 15:26:21 +0700

    as

--- Challenge1/AzureMobileApp/MobileClient.cs
+++ Challenge1/AzureMobileApp/MobileClient.cs
@@ -11,7 +11,7 @@ namespace AzureMobileApp
     public class MobileClient
     {
 
-        public static string ApplicationURL = @"https://mobile-85aa742d-c17a-4451-938f-d02d8f8c9eb1.azurewebsites.net";
+        public static string ApplicationURL = @"http://vnlala.azurewebsites.net";
         MobileServiceClient _client;
 
         private static MobileClient _mobileClient;
