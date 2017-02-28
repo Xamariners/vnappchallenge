de1ad1636e57869dbce148b4e2e744c7e0785970
lowleetak <lowleetak@gmail.com>
2017-02-28 15:45:04 +0700

    Completed iOS part

--- Challenge2/EmotionClient/iOS/ViewController.cs
+++ Challenge2/EmotionClient/iOS/ViewController.cs
@@ -6,8 +6,6 @@ namespace EmotionClient.iOS
 {
     public partial class ViewController : UIViewController
     {
-        int count = 1;
-
         public ViewController(IntPtr handle) : base(handle)
         {
         }
@@ -15,19 +13,38 @@ namespace EmotionClient.iOS
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-
-            // Perform any additional setup after loading the view, typically from a nib.
-            Button.AccessibilityIdentifier = "myButton";
-            Button.TouchUpInside += delegate {
-                var title = string.Format("{0} clicks!", count++);
-                Button.SetTitle(title, UIControlState.Normal);
-            };
         }
 
         public override void DidReceiveMemoryWarning()
         {
             base.DidReceiveMemoryWarning();
             // Release any cached data, images, etc that aren't in use.        
+        }
+
+        partial void TakePhotoButton_TouchUpInside(UIButton sender)
+        {
+            TakePhotoButton.Enabled = false;
+            UIImagePickerController picker = new UIImagePickerController();
+            picker.SourceType = UIImagePickerControllerSourceType.Camera;
+            picker.FinishedPickingMedia += async (o, e) => {
+                ThePhoto.Image = e.OriginalImage;
+                DetailsText.Text = "Processing...";
+                ((UIImagePickerController)o).DismissViewController(true, null);
+                using (var stream = e.OriginalImage.AsJPEG(.5f).AsStream())
+                {
+                    try
+                    {
+                        float happyValue = await Core.GetAverageHappinessScore(stream);
+                        DetailsText.Text = Core.GetHappinessMessage(happyValue);
+                    }
+                    catch (Exception ex)
+                    {
+                        DetailsText.Text = ex.Message;
+                    }
+                    TakePhotoButton.Enabled = true;
+                }
+            };
+            PresentModalViewController(picker, true);
         }
     }
 }
--- Challenge2/EmotionClient/iOS/ViewController.designer.cs
+++ Challenge2/EmotionClient/iOS/ViewController.designer.cs
@@ -1,24 +1,52 @@
-﻿//        
-// This file has been generated automatically by MonoDevelop to store outlets and        
-// actions made in the Xcode designer. If it is removed, they will be lost.        
-// Manual changes to this file may not be handled correctly.        
-//        
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
 
 namespace EmotionClient.iOS
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
         [Outlet]
         UIKit.UIButton Button { get; set; }
 
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel DetailsText { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton TakePhotoButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView ThePhoto { get; set; }
+
+        [Action ("TakePhotoButton_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void TakePhotoButton_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
         {
-            if (Button != null)
-            {
-                Button.Dispose();
-                Button = null;
+            if (DetailsText != null) {
+                DetailsText.Dispose ();
+                DetailsText = null;
+            }
+
+            if (TakePhotoButton != null) {
+                TakePhotoButton.Dispose ();
+                TakePhotoButton = null;
+            }
+
+            if (ThePhoto != null) {
+                ThePhoto.Dispose ();
+                ThePhoto = null;
             }
         }
     }
